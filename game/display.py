01cfc0366002bb7f76171ece2daf3a613fee8bd3
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-08-23 17:29:03 -0400

    change msvcrt for multiplataform solution

--- game/display.py
+++ game/display.py
@@ -2,7 +2,9 @@
 Este documento no requiere de cambios
 """
 
-import msvcrt
+import os
+import platform
+
 
 jn_to_string = {
     "j1": "Jugador 1",
@@ -28,5 +30,9 @@ def jugando(jn):
 
 def siguiente_jugada():
     print("\nPresiona una tecla para continuar...", end="", flush=True)
-    msvcrt.getch()
+    if platform.system() == "Windows":
+        os.system("pause >nul")
+    else:
+        os.system("/bin/bash -c 'read -s -n 1 -p \"\"'")
+    print()
     print("\n")
