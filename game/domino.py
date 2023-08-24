00e405b5ba702ff9b20645d799cef348177cf5f5
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-08-23 20:15:47 -0400

    refactor domino for testing, also added check for no chancho on start

--- game/domino.py
+++ game/domino.py
@@ -1,64 +1,84 @@
 from display import jugando, siguiente_jugada
 from begin_logic import determinar_jugador_inicial, repartir_fichas
-from in_game_logic import elegir_ficha, mostrar_fichas, jugar_ficha_al_mazo, generar_lista_de_jugadores
+from begin_logic import ficha_mas_alta
+from first_move import tiene_chancho
+from in_game_logic import (
+    elegir_ficha,
+    mostrar_fichas,
+    jugar_ficha_al_mazo,
+    generar_lista_de_jugadores,
+)
 from first_move import elegir_chancho
 
-while True:
-    try:
-        cantidad_jugadores = int(input("Indique la cantidad de jugadores: "))
-        if cantidad_jugadores > 1 and cantidad_jugadores < 15:
+
+def parse_players(cantidad_jugadores):
+    while True:
+        if not cantidad_jugadores.isdigit():  # check if the input is a number
+            print("\nEntrada no válida. Por favor, ingrese un número.")
+            print("\nIntente nuevamente.")
+            cantidad_jugadores = input("Indique la cantidad de jugadores: ")
+            continue
+
+        cantidad_jugadores = int(cantidad_jugadores)
+        if not (1 < cantidad_jugadores < 15):
+            print("\nLa cantidad de jugadores debe estar entre 2 y 14.")
+            print("\nIntente nuevamente.")
+            cantidad_jugadores = input("Indique la cantidad de jugadores: ")
+        else:
             break
-        print("La cantidad no es valida")
-    except:
-        print("No ingreso un valor numerico")
+    return cantidad_jugadores
 
-"""
-Descomente las siguientes 3 lineas, cuando begin_logic.py este completo, y
-orden_de_jugadores en in_game_logic.py este completado, ademas recuerde que
-debe borrar las otras 2 siguientes del comentario que indica el FAKE DATA..
-"""
-jugadores = repartir_fichas(cantidad_jugadores)
-jugador_inicial = determinar_jugador_inicial(jugadores)
-orden_de_jugadores = generar_lista_de_jugadores(jugador_inicial, cantidad_jugadores)
 
-print()
-fichas_jugadas = []  # es una lista donde estan toda la fila de fichas jugadas,
-# se agregan las fichas al inicio, o final de la lista..
-# recuerda que una ficha es lo siguiente: [nº1,nº2]
-ganador = None
-es_empate = 0
-while True:
-    for i in orden_de_jugadores:
-        print(f"{jugando(i)}, esta jugando..")
-        print("\nSu mano es la siguiente")
-        mostrar_fichas(jugadores[i])
-        if len(fichas_jugadas) == 0:
-            ficha_a_jugar = elegir_chancho(jugadores[i])
-            fichas_jugadas.append(ficha_a_jugar)
-        else:
-            ficha_a_jugar = elegir_ficha(fichas_jugadas, jugadores[i])
+def domino_game(cantidad_jugadores):
+    jugadores = repartir_fichas(cantidad_jugadores)
+    jugador_inicial = determinar_jugador_inicial(jugadores)
+    orden_de_jugadores = generar_lista_de_jugadores(jugador_inicial, cantidad_jugadores)
+
+    fichas_jugadas = []  # list of played domino pieces
+    ganador = None
+    es_empate = 0
+    while True:
+        for i in orden_de_jugadores:
+            print(f"{jugando(i)}, esta jugando..")
+            print("\nSu mano es la siguiente")
+            mostrar_fichas(jugadores[i])
+            if len(fichas_jugadas) == 0:
+                if tiene_chancho(jugadores[i]):
+                    ficha_a_jugar = elegir_chancho(jugadores[i])
+                else:
+                    ficha_a_jugar = ficha_mas_alta(jugadores[i])
+                fichas_jugadas.append(ficha_a_jugar)
+
+            else:
+                ficha_a_jugar = elegir_ficha(fichas_jugadas, jugadores[i])
+                if ficha_a_jugar is not None:
+                    fichas_jugadas = jugar_ficha_al_mazo(fichas_jugadas, ficha_a_jugar)
             if ficha_a_jugar is not None:
-                fichas_jugadas = jugar_ficha_al_mazo(fichas_jugadas, ficha_a_jugar)
-        if ficha_a_jugar is not None:
-            es_empate = 0
-            print(f"\nSe jugó lo siguiente: {ficha_a_jugar}")
-            jugadores[i].remove(ficha_a_jugar)
-            if len(jugadores[i]) == 0:
-                ganador = i
-                break
-        else:
-            es_empate += 1
-            print("\nEl jugador no tiene nada en la mano para jugar.")
-            if es_empate == cantidad_jugadores:
-                es_empate = True
-                break
-        print("\nEste es el nuevo estado actual de la mesa..")
-        mostrar_fichas(fichas_jugadas)
-        siguiente_jugada()
+                es_empate = 0
+                print(f"Se jugó lo siguiente: {ficha_a_jugar}")
+                jugadores[i].remove(ficha_a_jugar)
+                if len(jugadores[i]) == 0:
+                    ganador = i
+                    break
+            else:
+                es_empate += 1
+                print("El jugador no tiene nada en la mano para jugar.")
+                if es_empate == cantidad_jugadores:
+                    es_empate = True
+                    break
+            print("\nLas siguientes fichas estan en mesa..")
+            mostrar_fichas(fichas_jugadas)
+            siguiente_jugada()
 
-    if ganador is not None:
-        print(f"\n¡El {jugando(ganador)} es el ganador de esta partida!")
-        break
-    elif es_empate:
-        print("\n¡Ningún jugador es el ganador de esta partida!")
-        break
+        if ganador is not None:
+            print(f"\n¡El {jugando(ganador)} es el ganador de esta partida!")
+            break
+        elif es_empate:
+            print("\n¡Ningún jugador es el ganador de esta partida!")
+            break
+
+
+if __name__ == "__main__":
+    user_input = input("Indique la cantidad de jugadores: ")
+    cantidad_jugadores = parse_players(user_input)
+    domino_game(cantidad_jugadores)
